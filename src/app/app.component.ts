3c6fbe7b7e752dc1a76d861a869967a48ca9e0ec
Dharmen Shah <dharmen.s@milestoneinternet.com>
2020-04-26 20:25:54 +0530

    make both options available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   template: `
+    <h1>Load replies through <i>ng-container</i> and <i>ng-template</i></h1>
+    <ng-container
+      *ngTemplateOutlet="replyThread; context: { $implicit: replies }"
+    ></ng-container>
+    <ng-template #replyThread let-childReplies>
+      <ul>
+        <li *ngFor="let reply of childReplies">
+          <b>{{ reply.id }}:</b> {{ reply.value }}
+          <ng-container *ngIf="reply.children">
+            <ng-container
+              *ngTemplateOutlet="
+                replyThread;
+                context: { $implicit: reply.children }
+              "
+            ></ng-container>
+          </ng-container>
+        </li>
+      </ul>
+    </ng-template>
+    <hr />
+    <h1>Load replies through <i>app-replay</i> component</h1>
     <app-reply [replies]="replies"></app-reply>
-  `
+  `,
+  styles: ["ul { list-style: none }"],
 })
 export class AppComponent {
   replies = [
